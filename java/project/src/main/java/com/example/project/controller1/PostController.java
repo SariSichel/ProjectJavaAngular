7c6aa594ec3eace6466d2a2210bf31df3d6eb746
SariSichel <sarisichel4@gmail.com>
2025-10-29 00:10:36 +0200

    add controller1

---
+++ java/project/src/main/java/com/example/project/controller1/PostController.java
@@ -0,0 +1,66 @@
+package com.example.project.controller;
+
+import com.example.project.dto.PostDTO;
+import com.example.project.mappers.CategoryMapper;
+import com.example.project.mappers.CommentMapper;
+import com.example.project.mappers.PostMapper;
+import com.example.project.mappers.UserNameMapper;
+import com.example.project.model.Post;
+import com.example.project.service.PhotoUtils;
+import com.example.project.service.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/Post")
+@CrossOrigin
+public class PostController {
+    PostRepository postRepository;
+    PostMapper postMapper;
+    //לבדוק אם חייבים את הזרקות האלו
+    UserNameMapper userNameMapper;
+    CategoryMapper categoryMapper;
+    CommentMapper commentMapper;
+    @Autowired
+    public PostController(PostRepository postRepository, PostMapper postMapper, UserNameMapper userNameMapper, CategoryMapper categoryMapper, CommentMapper commentMapper) {
+        this.postRepository = postRepository;
+        this.postMapper = postMapper;
+        this.userNameMapper = userNameMapper;
+        this.categoryMapper = categoryMapper;
+        this.commentMapper = commentMapper;
+    }
+
+
+
+@GetMapping("/getPostById/{id}")
+    public ResponseEntity<PostDTO> getPostById(@PathVariable long id){
+    try{
+        Post p=postRepository.findById(id).get();
+        if(p==null){
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(postMapper.postToDTO(p, userNameMapper, categoryMapper, commentMapper), HttpStatus.OK);
+    } catch (Exception e) {
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
+@PostMapping("/addPost")
+    public ResponseEntity<Post> addPost(@RequestPart("photo") MultipartFile photo, @RequestPart("post") Post p, @RequestPart("audio") MultipartFile audio){
+        try{
+            PhotoUtils.uploadImage(photo);
+            p.setPhotoPath((photo.getOriginalFilename()));
+            Post post=postRepository.save(p);
+            return new ResponseEntity<>(post, HttpStatus.CREATED);
+            //לסדר את האודיו
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+}
+
+
+}
---
+++ java/project/src/main/java/com/example/project/controller1/UserController.java
@@ -0,0 +1,56 @@
+package com.example.project.controller;
+
+import com.example.project.dto.PostDTO;
+import com.example.project.mappers.*;
+import com.example.project.model.Users;
+import com.example.project.service.PhotoUtils;
+import com.example.project.service.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+@RestController
+@RequestMapping("/api/User")
+@CrossOrigin
+public class UserController {
+    UsersRepository userRepository;
+    UserSignUpMapper userSignUpMapper;
+    @Autowired
+    public UserController(UsersRepository userRepository, UserSignUpMapper userSignUpMapper) {
+        this.userRepository = userRepository;
+        this.userSignUpMapper = userSignUpMapper;
+    }
+
+
+
+
+
+
+    @GetMapping("/getUserById/{id}")
+    public ResponseEntity<PostDTO> getUserById(@PathVariable long id){
+        try{
+            Users u=userRepository.findById(id).get();
+            if(u==null){
+                return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(UserSignUpMapper.userToUserSignUpDTO(u, HttpStatus.OK));
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/addUser")
+    public ResponseEntity<Users> addUser(@RequestPart("photo") MultipartFile photo, @RequestPart("post") Users u){
+        try{
+            PhotoUtils.uploadImage(photo);
+            u.setPhotoPath((photo.getOriginalFilename()));
+            Users u1=userRepository.save(u);
+            return new ResponseEntity<>(u1, HttpStatus.CREATED);
+            //לסדר את האודיו
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+}
+    }
